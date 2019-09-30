f947d70e610679bd10ef5bb7ba658617a0490a7b
Rick Leir <rleir@leirtech.com>
2019-09-30 14:14:40 -0400

    now zoomable, using leaflet and geodesic

--- src/site_conn/conn_map.js
+++ src/site_conn/conn_map.js
@@ -1,55 +1,57 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-// The svg
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+	var map = L.map('map').setView([30.5, -0.09], 2);
 
-// Map and projection
-var projection = d3.geoMercator()
-    .scale(85)
-    .translate([width/2, height/2*1.3])
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
 
-// A path generator
-var path = d3.geoPath()
-    .projection(projection)
+	function onEachFeature(feature, layer) {
+		var popupContent = "";
 
-// Load world shape AND list of connection
-d3.queue()
-  .defer(d3.json, "world.geojson")  // World shape
-  .defer(d3.csv,  "loan_conns.csv") // Position of circles data_connectionmap.csv
-  .await(ready);
+		if (feature.properties) {
+            if( feature.properties.place) {
+			    popupContent += feature.properties.place;
+			    popupContent += "<br>";
+            }
+            // zzz insti names in data
+            if( feature.properties.popupContent) {
+			    popupContent += "<ul>";
+                for(var key in feature.properties.popupContent) {
+                    popupContent += "<li>";
+			        popupContent += feature.properties.popupContent[key] + " - ";
+			        popupContent += key;
+			        popupContent += "</li>";
+                };
+			    popupContent += "</ul>";
+			}
+        }
+		layer.bindPopup(popupContent);
+	}
 
-function ready(error, dataGeo, data) {
+	L.geoJSON([connData], {
 
-    // Reformat the list of link. Note that columns in csv file are called long1, long2, lat1, lat2
-    var link = []
-    data.forEach(function(row){
-      source = [+row.long1, +row.lat1]
-      target = [+row.long2, +row.lat2]
-      topush = {type: "LineString", coordinates: [source, target], loan_count: row.loan_count}
-      link.push(topush)
-    })
+		style: function (feature) {
+			return feature.properties && feature.properties.style;
+		},
 
-    // Draw the map
-    svg.append("g")
-        .selectAll("path")
-        .data(dataGeo.features)
-        .enter().append("path")
-            .attr("fill", "#b8b8b8")
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            .style("stroke", "#fff")
-            .style("stroke-width", 0)
+		onEachFeature: onEachFeature,
 
-    // Add the path
-    svg.selectAll("myPath")
-      .data(link)
-      .enter()
-      .append("path")
-        .attr("class", "loan-conns")
-        .attr("d", function(d){ return path(d)})
-        .style("fill", "none")
-        .style("stroke", "#69b3a2")
-        .style("stroke-width", function(d){ return d.loan_count})
-}
+		pointToLayer: function (feature, latlng) {
+
+            // using Henry Thasler's library
+            // https://www.thasler.com/leaflet.geodesic/example/interactive-noWrap.html
+            
+            // an arc from ottawa to latlng
+            return L.geodesic([[[45.421, -75.697], latlng]], {
+			    weight: 7,
+			    opacity: 0.5,
+			    color: 'blue',
+			    steps: 50,
+			    wrap: false
+		    });
+		}
+	}).addTo(map);
+
+});
+
